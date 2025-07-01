2483609595a31fead10b1bde8508fdb9d394a7e7
Uvesh99 <sayaidisrar@gmail.com>
2025-07-01 20:56:17 +0530

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -47,4 +47,6 @@ app.get("/",(req,res)=>{
 // Socket.IO
 initSocket(server);
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// src/index.js
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
